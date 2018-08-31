88e2cc9745b030f0a54f3ac51b2f33140b8e853b
Michael Tomae <michael.tomae@gmail.com>
2018-08-31 00:44:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ function displayFiveDayForecast(json) {
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
   const ctx = document.getElementById('WeatherChart').getContext('2d')
+  const labels = json.
 }
 
 document.addEventListener('DOMContentLoaded', function() {
