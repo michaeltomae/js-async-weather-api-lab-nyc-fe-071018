d45f4c6e0ea92afb7f9be18326096d8be9a9fa1c
Michael Tomae <michael.tomae@gmail.com>
2018-08-31 00:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
   console.log(json)
   const forecast = json.listenerforecast.forEach((eachForecastItem) => {
-    
+    const div = document.createElement('div')
+    div.innerHTML = 
   })
 }
 
